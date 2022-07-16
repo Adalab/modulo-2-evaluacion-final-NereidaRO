2ccacc28878dd8fb0309a5f0a002c7dfdc9a6e1c
Nereida Rodríguez <nereida@LAPTOP-VAJBTHLP.localdomain>
2022-07-16 15:08:10 +0200

    added basic HTML and global vars

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+//Variables globales
+const searchListHTML = document.querySelector('.js-search-list');
+const favListHTML = document.querySelector('.js-fav-list');
+const form = document.querySelector('.js-form');
+const inputSearch = document.querySelector('.js-input-search');
+const buttonSearch = document.querySelector('.js-button-search');
+const buttonReset = document.querySelector('.js-button-reset');
+
+//Arrays principales
+let searched = [];
+let favourites = [];
+
+//Relacionado al servidor
+
+
+//Al cargar la página
