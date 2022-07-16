1b3cc8fa159010a3976752146be612f2275675a7
Nereida Rodríguez <nereida@LAPTOP-VAJBTHLP.localdomain>
2022-07-16 19:32:27 +0200

    started the fetch issue and doing the render function

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
-console.log('>> Ready :)');
+console.log('The script is linked!');
 
 //Variables globales
+
 const searchListHTML = document.querySelector('.js-search-list');
 const favListHTML = document.querySelector('.js-fav-list');
 const form = document.querySelector('.js-form');
@@ -11,10 +12,24 @@ const buttonSearch = document.querySelector('.js-button-search');
 const buttonReset = document.querySelector('.js-button-reset');
 
 //Arrays principales
-let searched = [];
+
+let foundedAnimes = [];
 let favourites = [];
 
 //Relacionado al servidor
+
+function renderAnime () {}; //tal vez esta va al cargar
+
+function searchAnime () {
+    const searchValue = inputSearch.value;
+    fetch(`https://api.jikan.moe/v4/anime?q=${searchValue}`)
+    .then ((response) => response.json)
+    .then ((data) => {
+        foundedAnimes.push(data);
+        //hay que renderizar lo que devuelva el server: solo el título y la img y en lugares concretos de la tarjeta
+    });
+
+}
 
 
 //Al cargar la página
